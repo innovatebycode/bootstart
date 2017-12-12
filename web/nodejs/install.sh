7bfe291a2461d26b30d02749e22cccd17999665c
innovatebycode <34467140+innovatebycode@users.noreply.github.com>
2017-12-11 21:13:08 -0800

    Update install.sh
    added the version check command

--- web/nodejs/install.sh
+++ web/nodejs/install.sh
@@ -1,4 +1,7 @@
+
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install nodejs
+node -v
+npm -v
 
